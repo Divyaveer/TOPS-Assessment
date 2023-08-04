a3468419670360513f55458be6215c4c49d4e4b8
Divyaveer <86354945+Divyaveer@users.noreply.github.com>
2023-08-04 18:44:37 +0530

    Divyaveer

--- Food_Billing/food_billing.c
+++ Food_Billing/food_billing.c
@@ -24,6 +24,7 @@ int main() {
     strcpy(item[5].name, "Dal Fry");
     strcpy(item[6].name, "Kadai Paneer");
     strcpy(item[7].name, "Mix Veg");
+    strcpy(item[8].name, "Veg Manchurian Spicy");
 
     item[0].price = 180;
     item[1].price = 100;
@@ -33,14 +34,15 @@ int main() {
     item[5].price = 110;
     item[6].price = 135;
     item[7].price = 90;
+    item[8].price = 200;
 
     do{
         printf("\n*************************************************************************************************************************");
         printf("\n\t\t\t\t\t\t\t\tMENU \t\t\t");
         printf("\n*************************************************************************************************************************\n");
-        printf("\tSr.No.\t|\tItem(s)\t\t\t|\tRate (Rs.)");
+        printf("\tSr.No.\t|\tItem(s)\t\t\t|\tRate (Rs./Plate)");
         printf("\n*************************************************************************************************************************\n");
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < 9; i++){
             if(strlen(item[i].name) < 6){
                 printf("\t%d\t| %s\t\t\t\t|\t%d\n", i+1, item[i].name, item[i].price);
             }else if(strlen(item[i].name) > 14){
@@ -55,7 +57,7 @@ int main() {
         printf("Enter what you want to Eat (Enter Index of each you want to purchase one by one) : ");
         scanf("%d", &choice);
 
-        if(choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6 || choice == 7 || choice == 8){
+        if(choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6 || choice == 7 || choice == 8 || choice == 9){
             printf("\nYou have selected %s.\n", item[choice-1].name);
             printf("Enter the Quantity : ");
             scanf("%d", &order[selectedItems].quantity);
@@ -78,9 +80,9 @@ int main() {
             continue;
         }else{
             printf("\n**********************************************************************************************************************************************************************");
-            printf("\n\t\t\t\t\t\t\t\tBilling \t\t\t");
+            printf("\n\t\t\t\t\t\t\t\tBill\t\t\t");
             printf("\n*********************************************************************************************************************************************************************\n");
-            printf("\tSr.No.\t|\tItem(s)\t\t\t|\tRate (Rs.)\t\t|\tQuantity\t\t|\tAmount (Rs.)");
+            printf("\tSr.No.\t|\tItem(s)\t\t\t|\tRate (Rs/Plate)\t\t|\tQuantity\t\t|\tAmount (Rs.)");
             printf("\n*********************************************************************************************************************************************************************\n");
             for(int i = 0; i < selectedItems; i++){
                 if(strlen(order[i].name) < 6){
@@ -94,7 +96,7 @@ int main() {
                 }
             }
             printf("\n*********************************************************************************************************************************************************************\n");
-            printf("Total = %d\n", total);
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t\t\tTotal = Rs.%d\n", total);
             printf("\n*********************************************************************************************************************************************************************\n");
             break;
         }
